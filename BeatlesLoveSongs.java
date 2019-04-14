cd890cc12264d631ab0c45df0fd0ec5aa7d61642
Kristy <mengxueli2015@gmail.com>
2019-04-14 16:17:36 -0400

    change return error to 1

--- BeatlesLoveSongs.java
+++ BeatlesLoveSongs.java
@@ -11,7 +11,7 @@ public class BeatlesLoveSongs {
 		int index1 = s.indexOf(c);
 		String remainString = s.substring(index1 + 1);
 		if (remainString.indexOf(c) == -1){
-			return null;
+			return "1";
 		}
 		else{
 			return remainString.substring(0, remainString.indexOf(c));
